ce7ceeca5e8e28b6f27c4c2ffc63264c9dc532e0
aleksanderlj <aleksanderlj@hotmail.com>
2021-11-03 13:15:36 +0100

    Signin export default thingy

--- web/src/profileUI/SignIn.js
+++ web/src/profileUI/SignIn.js
@@ -3,35 +3,34 @@ import GoogleLogin, {GoogleLogout} from "react-google-login";
 
 const clientId = "191430355915-jgfp9tt0cacihubuggmuqoooqolooord.apps.googleusercontent.com";
 
-const onSuccess = (res) => {
-    console.log("Login success for: ", res.profileObj);
-};
+export default function SignIn(){
+    const onSuccess = (res) => {
+        console.log("Login success for: ", res.profileObj);
+    };
 
-const onFailure = (res) => {
-    console.log("Login go nono", res);
-};
+    const onFailure = (res) => {
+        console.log("Login go nono", res);
+    };
 
-const onLogoutSuccess = (res) => {
-    console.log("Logout success")
-};
+    const onLogoutSuccess = (res) => {
+        console.log("Logout success")
+    };
 
-const onLogoutFailure = (res) => {
-    console.log("Logout go nono", res);
-};
+    const onLogoutFailure = (res) => {
+        console.log("Logout go nono", res);
+    };
 
-const SignIn = () => {
     return(
         <div>
             <GoogleLogin clientId={clientId}
-            buttonText="Login"
-            onSuccess={onSuccess}
-            onFailure={onFailure}/>
+                         buttonText="Login"
+                         onSuccess={onSuccess}
+                         onFailure={onFailure}
+                         cookiePolicy={'single_host_origin'}/>
             <GoogleLogout clientId={clientId}
-            buttonText="Logout"
-            onLogoutSuccess={onLogoutSuccess}
-            onFailure={onLogoutFailure}/>
+                          buttonText="Logout"
+                          onLogoutSuccess={onLogoutSuccess}
+                          onFailure={onLogoutFailure}/>
         </div>
     )
 }
-
-export default SignIn;
