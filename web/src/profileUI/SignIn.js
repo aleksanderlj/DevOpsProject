550837aff94b5a8f7c9d046ca33973e8c6f863a6
aleksanderlj <aleksanderlj@hotmail.com>
2021-11-05 16:47:44 +0100

    Sign in with google

--- web/src/profileUI/SignIn.js
+++ web/src/profileUI/SignIn.js
@@ -2,10 +2,21 @@ import React from "react";
 import GoogleLogin, {GoogleLogout} from "react-google-login";
 
 const clientId = "191430355915-jgfp9tt0cacihubuggmuqoooqolooord.apps.googleusercontent.com";
+const {REACT_APP_MONGODB} = process.env;
 
 export default function SignIn(){
     const onSuccess = (res) => {
         console.log("Login success for: ", res.profileObj);
+        console.log("Send to backend: ", res.getAuthResponse().id_token);
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'text/plain'},
+            body: res.getAuthResponse().id_token
+        };
+        console.log(REACT_APP_MONGODB);
+        fetch( process.env.REACT_APP_MONGODB + "/user/login/google", requestOptions)
+            .then(response => response.text())
+            .then(data => console.log(data));
     };
 
     const onFailure = (res) => {
